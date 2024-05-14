b0d2eaaee274d6fdfd2b4e969e526beac1bd0cb8
Joel Lau <joellau@protonmail.com>
2024-05-14 22:39:31 +0800

    plugins

--- src/app/shared/state/store.module.ts
+++ src/app/shared/state/store.module.ts
@@ -1,6 +1,9 @@
-import { NgModule } from '@angular/core';
-import { NgxsModule } from '@ngxs/store';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { MoviesState } from './movies/movies.state';
 import { environment } from '@env/environment';
 
@@ -12,6 +15,9 @@ export const NGXS_STATES = [MoviesState];
     NgxsModule.forRoot(NGXS_STATES, {
       developmentMode: !environment.production,
     }),
+    NgxsStoragePluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
+    NgxsFormPluginModule.forRoot(),
   ],
   exports: [NgxsModule],
 })
