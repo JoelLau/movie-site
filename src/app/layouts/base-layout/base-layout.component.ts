dc5d7d12127c5056a6f702c7aa528a9c7cbeaf06
Joel Lau <joellau@protonmail.com>
2024-05-17 06:15:08 +0800

    footer

--- src/app/layouts/base-layout/base-layout.component.ts
+++ src/app/layouts/base-layout/base-layout.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LastVisitedFooterComponent } from '@components/last-visited-footer/last-visited-footer.component';
 import { SideMenuComponent } from '@components/side-menu/side-menu.component';
+import { Movies } from '@shared/state/movies/movies.models';
 
 @Component({
   imports: [
@@ -11,10 +13,14 @@ import { SideMenuComponent } from '@components/side-menu/side-menu.component';
 
     //Custom
     SideMenuComponent,
+    LastVisitedFooterComponent,
   ],
   selector: 'app-base-layout',
   standalone: true,
   styleUrl: './base-layout.component.scss',
   templateUrl: './base-layout.component.html',
 })
-export class BaseLayoutComponent {}
+export class BaseLayoutComponent {
+  @Input()
+  lastVisitedMovies?: Movies;
+}
